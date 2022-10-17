2561cc45dabd5e0bc456d3405df616fe4a1de10d
Jakub Bureš <jak.bures@gmail.com>
2022-10-17 12:14:51 +0200

    Create asset junction while initializing on windows

--- initialize.bat
+++ initialize.bat
@@ -1,10 +1,16 @@
 @echo off
 if "%~1"=="" goto fail
-set target="%~1"
+set target=%~1
 
-echo Target folder is: %target%
+echo Target folder is: "%target%"
 if not exist "%target%" mkdir "%target%"
 cmake -S . -B "%target%"
+
+if not exist "%target%/assets" (
+	mklink /j "%target%/assets" "./assets"
+)
+
+exit
 
 :fail
 @echo Please, specify build folder
